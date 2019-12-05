4a2e0dd72b4958a6fe92de144210a9120f14ca1c
Keith Buel <keith.buel@dealer.com>
2019-12-04 21:36:33 -0500

    more ie 11

--- test/app/src/webpackEntry.js
+++ test/app/src/webpackEntry.js
@@ -1,35 +1,42 @@
-/* entry point for SPA application */
-/* global window, document */
-import TestApp from './testApp';
-import { getDefaultConfig, getConfigFromUrl, getConfigFromStorage, clearStorage } from './config';
+"use strict";
 
-let app;
-let config;
-const rootElem = document.getElementById('root');
+var _testApp = _interopRequireDefault(require("./testApp"));
+
+var _config = require("./config");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+/* entry point for SPA application */
+
+/* global window, document */
+var app;
+var config;
+var rootElem = document.getElementById('root');
 
 function mount() {
   // Create the app as a function of config
-  app = new TestApp(config);
+  app = new _testApp.default(config);
   window._testApp = app; // Expose for console fiddling
+
   app.mount(window, rootElem);
   return app;
-}
+} // Regular landing, read config from URL
 
-// Regular landing, read config from URL
-window.bootstrapLanding = function() {
-  config = window.location.search ? getConfigFromUrl() : getDefaultConfig();
+
+window.bootstrapLanding = function () {
+  config = window.location.search ? (0, _config.getConfigFromUrl)() : (0, _config.getDefaultConfig)();
   mount();
   app.bootstrapHome();
-};
+}; // Callback, read config from storage
 
-// Callback, read config from storage
-window.bootstrapCallback = function() {
-  config = getConfigFromStorage();
-  clearStorage();
+
+window.bootstrapCallback = function () {
+  config = (0, _config.getConfigFromStorage)();
+  (0, _config.clearStorage)();
   mount();
   app.bootstrapCallback();
 };
 
-window.addEventListener('load', () => {
+window.addEventListener('load', function () {
   rootElem.classList.add('loaded');
 });
