107511345dc7369ba50c538cabc140c250e7db71
Keith Buel <keith.buel@dealer.com>
2019-12-06 14:34:44 -0500

    babel again?

--- test/app/webpack.config.js
+++ test/app/webpack.config.js
@@ -34,12 +34,16 @@ module.exports = {
   devtool: 'source-map',
   module: {
     rules: [
+      { 
+	test: /\.js$/, 
+	exclude: /node_modules/, 
+	use: ["babel-loader"]
+      },
       {
         test: /\.js$/,
         use: ['source-map-loader'],
         enforce: 'pre'
-      },
-      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+      }
     ]
   }
 };
